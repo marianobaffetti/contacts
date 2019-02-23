2008a587e7b258ab2e0e822f23e999d999528ec3
marianobaffetti <marianobaffetti@gmail.com>
2019-02-23 19:12:50 -0300

    Se agrega funcionalidad para obtener lista de contactos en api externa.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import ContactsList from './ContactsList';
 
 class App extends Component {
-  render() {
-    return (
-      <ContactsList />
-    );
+  constructor(props){
+    super(props);
+    this.state = {
+      contactsInfo : [],
+      loading: true
+    }
   }
-}
 
+  render() {
+    if (this.state.loading){
+      return 'loading...'
+    } else {
+      return (
+        <ContactsList contactsInfo = {this.state.contactsInfo} />
+      );
+    }
+  }
+  componentWillMount(){
+     fetch('http://s3.amazonaws.com/technical-challenge/v3/contacts.json')  
+     .then(response => response.json())
+       .then(contacts =>{
+           this.setState({ contactsInfo: contacts, loading: false }) ;
+     })
+   }
+}
 export default App;
